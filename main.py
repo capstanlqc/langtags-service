6c56823ac8510b1c3050c10859f01dab6c437dfc
Manuel Souto Pico <m.soutopico@gmail.com>
2024-07-12 01:15:02 +0200

    Added port to python file

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello, world!"}
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)

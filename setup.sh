788114e3929d078d6c32c34d01a3d7dd19b1858d
Manuel Souto Pico <m.soutopico@gmail.com>
2025-04-01 22:08:55 +0200

    force glibc compatibility

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-#!/bin/bash
-nix profile install nixpkgs#glibc --extra-experimental-features flakes
-export LD_LIBRARY_PATH=$HOME/.nix-profile/lib:$LD_LIBRARY_PATH
+mkdir -p /opt/glibc-2.38
+cd /opt/glibc-2.38
+curl -LO http://ftp.gnu.org/gnu/libc/glibc-2.38.tar.gz
+tar -xzf glibc-2.38.tar.gz
+cd glibc-2.38
+mkdir build
+cd build
+../configure --prefix=/opt/glibc-2.38
+make -j$(nproc)
+make install
+export LD_LIBRARY_PATH=/opt/glibc-2.38/lib:$LD_LIBRARY_PATH
 
 apt-get update
 # apt-get update && apt-get install -y libc6=2.38-1

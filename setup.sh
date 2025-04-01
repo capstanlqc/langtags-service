f49f8ff1f92742cb7d8fc744d632d1caaa64278e
Manuel Souto Pico <m.soutopico@gmail.com>
2025-04-01 22:06:51 +0200

    add flakes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-nix profile install nixpkgs#glibc
+#!/bin/bash
+nix profile install nixpkgs#glibc --extra-experimental-features flakes
 export LD_LIBRARY_PATH=$HOME/.nix-profile/lib:$LD_LIBRARY_PATH
+
 apt-get update
 # apt-get update && apt-get install -y libc6=2.38-1
 apt-get install -y openjdk-21-jdk ant

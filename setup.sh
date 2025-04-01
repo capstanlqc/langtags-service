d6a661426ac3811833becbad00d1c1383fb26dc4
Manuel Souto Pico <m.soutopico@gmail.com>
2025-04-01 22:01:28 +0200

    Install install nix profile

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-apt-get update && apt-get install -y libc6=2.38-1
+nix profile install nixpkgs#glibc
+export LD_LIBRARY_PATH=$HOME/.nix-profile/lib:$LD_LIBRARY_PATH
+apt-get update
+# apt-get update && apt-get install -y libc6=2.38-1
 apt-get install -y openjdk-21-jdk ant

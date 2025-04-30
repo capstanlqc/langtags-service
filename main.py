04c5182b5dc7023c062c695b0717899ab3a559f2
Manuel Souto Pico <m.soutopico@gmail.com>
2025-04-30 10:38:14 +0200

    added missing imports and functions

--- main.py
+++ main.py
@@ -1,10 +1,108 @@
 import os
 import subprocess
+import zipfile
+import requests
+
 from pathlib import Path
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
+def download_file(url, dest_fpath):
+    """
+    Download a large file with streaming.
+
+    :param url: URL of the file to download.
+    :param destination: Path to local version of file that will be written.
+    """
+    try:
+        with requests.get(url, stream=True) as response:
+            response.raise_for_status()  # Raise an error for bad HTTP status
+            with open(dest_fpath, "wb") as file:
+                for chunk in response.iter_content(chunk_size=8192):  # 8 KB chunks
+                    if chunk:  # Filter out keep-alive chunks
+                        file.write(chunk)
+        # print(f"Download completed: {dest_fpath}")
+    except Exception as e:
+        # todo: raise exception
+        print(f"An error occurred: {e}")
+
+
+def install_config_bundle(custom_config_url, config_dpath):
+    
+    os.makedirs(config_dpath, mode=0o777, exist_ok=True)
+
+    def config_is_outdated():
+        
+        def get_version(line):
+            update_pattern = re.compile(r"(\d+)_[csp0]{3}")
+            result = update_pattern.search(line.strip())
+            return result.group(1)
+            
+        # check if it has not been installed already
+        local_version_fpath = os.path.join(config_dpath, "version_notes.txt")
+        if not os.path.isfile(local_version_fpath):
+            print("Custom configuration not found, configuration will be installed")
+            return True
+        
+        with open(local_version_fpath, "r") as f:
+            # read the first line of the file
+            update_line = f.readlines()[2]
+            installed_version = get_version(update_line)
+
+        dest_fpath = os.path.join(config_dpath, "index.html")
+        download_file(custom_config_url, dest_fpath)
+
+        with open(dest_fpath, "r") as file:
+            # read the first line of the file
+            update_line = file.readlines()[1]
+            available_version = get_version(update_line)
+            
+        if installed_version < available_version:
+            print("Custom configuration is outdated, installing config")
+            return True
+        
+        print("Custom configuration is up to date")
+        return
+
+    if not config_is_outdated():
+        return config_dpath
+
+    # these urls can be fetched from the file above (index.html)
+    config = f"{custom_config_url}/manual/config.zip"
+    plugins = f"{custom_config_url}/manual/plugins.zip"
+    scrips = f"{custom_config_url}/manual/scripts.zip"
+
+    # config_bundle = "https://github.com/capstanlqc/omegat-user-config-dev572/archive/refs/heads/master.zip"
+
+    #@ download_file
+    def fetch_file(url, folder):
+        if not os.path.isdir(folder):
+            os.makedirs(folder, mode=0o777, exist_ok=True)
+
+        zip_fname = url.split("/")[-1]
+        zip_fpath = os.path.join(folder, zip_fname)
+        download_file(url, zip_fpath)
+        
+        with zipfile.ZipFile(zip_fpath, "r") as zip_ref:
+            zip_ref.extractall(folder)
+
+        if os.path.isfile(zip_fpath):
+            os.remove(zip_fpath)
+
+    fetch_file(config, config_dpath)
+    fetch_file(plugins, os.path.join(config_dpath, "plugins"))
+    fetch_file(scrips, os.path.join(config_dpath, "scripts"))
+
+    # print(f"save file to '{config_dpath}'")
+    # fetch_file(config_bundle, dist_dir)
+
+    required = ["scripts", "plugins", "omegat.prefs", "version_notes.txt"]
+    # set(required).issubset(set(os.listdir(config_dpath)))
+    if all(item in os.listdir(config_dpath) for item in required):
+        return config_dpath
+        
+        
 APP_ROOT = Path.cwd()
 
 load_dotenv()
